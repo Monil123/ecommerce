6781dea111e9e96a2220a79abac77f8627ebca08
Monil Ladha <monil.ladha@reqruto.com>
2021-01-23 12:07:02 +0530

    First commit

---
+++ src/main/java/ecommerce/controller/LoginController.java
@@ -0,0 +1,14 @@
+package ecommerce.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class LoginController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
